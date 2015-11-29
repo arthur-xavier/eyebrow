25dc67798db97402102ed996a499907deabebac6
Arthur Xavier <arthur.xavierx@gmail.com>
2015-11-29 17:58:45 -0200

    Clean up todomvc sample html and added comments to todomvc todos view file

--- samples/todomvc/js/views/todos.js
+++ samples/todomvc/js/views/todos.js
@@ -4,8 +4,18 @@
   var KEY_ENTER = 13,
       KEY_ESCAPE = 27;
 
+  /**
+   * Main View
+   * @param {Array}  store  Stored todos
+   * @param {string} filter
+   */
   var TodosView = function(store, filter) {
-    // view events
+
+    /**
+     * Creates a new Todo and adds it to store
+     * Called on keyup event of input#new-todo
+     * @param {Array} store
+     */
     this.create = function(store) {
       var $input = event.target;
       var val = $input.value.trim();
@@ -20,24 +30,46 @@
         completed: false
       });
       $input.value = "";
+
+      Brow();
     };
 
+    /**
+     * Displays edit input field for todo
+     * Called on dblclick event of label
+     * @param {Array}  store
+     * @param {string} id
+     */
     this.edit = function(store, id) {
       var item = Brow.$('[data-id="' + id + '"');
       item.className += ' editing';
       Brow.$('.edit', item).focus();
     };
 
+    /**
+     * Checks for Enter or Escape key presses
+     * If Escape key is pressed, cancel the todo edition
+     * If Enter key is pressed, update the todo
+     * Called on keyup event of input.edit
+     */
     this.keyup = function() {
+      var $input = event.target;
+
       if(event.which === KEY_ENTER) {
-        event.target.dataset.abort = false;
-        event.target.blur();
+        $input.dataset.abort = false;
+        $input.blur();
       } else if(event.which === KEY_ESCAPE) {
-        event.target.dataset.abort = true;
-        event.target.blur();
+        $input.dataset.abort = true;
+        $input.blur();
       }
     };
 
+    /**
+     * Updates the title of a todo
+     * Called on focusout event of input.edit
+     * @param {Array}  store
+     * @param {string} id
+     */
     this.update = function(store, id) {
       var $input = event.target;
       var val = $input.value;
@@ -51,21 +83,47 @@
       } else {
         $input.dataset.abort = false;
       }
+
+      Brow();
     };
 
+    /**
+     * Removes a todo from the list
+     * Called on click event of button.destroy
+     * @param {Array}  store
+     * @param {string} id
+     */
     this.destroy = function(store, id) {
       store.splice(utils.findTodoIndexById(id), 1);
+      Brow();
     };
 
+    /**
+     * Marks a todo as completed
+     * @param {Array}  store
+     * @param {string} id
+     */
     this.mark = function(store, id) {
-      var i = utils.findTodoIndexById(id);
-      store[i].completed = !store[i].completed;
+      store[utils.findTodoIndexById(id)].completed ^= true;
+      Brow();
     };
 
+    /**
+     * Marks all todos as completed
+     * Called on change event of input#toggle-all
+     * @param {Array} store
+     */
     this.mark_all = function(store) {
       store.forEach(function(t) { t.completed = event.target.checked; });
+      Brow();
     };
 
+    /** 
+     * Removes all completed todos from the list
+     * Called on click event of button#clear-completed
+     * @param  {Array} store
+     * @return {Array} new store with completed todos removed
+     */
     this.clear_completed = function(store) {
       return store.filter(function(t) { return !t.completed; });
     };
